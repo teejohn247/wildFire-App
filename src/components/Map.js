70fae707b6ce1790a553a48dc13344deebde9a8d
teejohn247 <teejohn247@gmail.com>
2020-12-31 01:17:11 +0100

    added location info

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import {React, useState} from 'react'
 import GoogleMapReact from 'google-map-react';
-import LocationMarker from './LocationMarker'
+import LocationMarker from './LocationMarker';
+import LocationInfoBox from './LocationInfoBox';
 
 const Map = ({eventData, center, zoom}) => {
-    const markers = eventData.map(ev => {
+    const [locationInfo, setLocationInfo] = useState(false)
+    const markers = eventData.map((ev, index) => {
         if(ev.categories[0].id === 8){
-            return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+            return <LocationMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} 
+            onClick={() => setLocationInfo({
+               id: ev.id,
+               title:ev.title
+            })}/>
         }
         return null
     })
@@ -16,8 +22,10 @@ const Map = ({eventData, center, zoom}) => {
             defaultZoom = {zoom}>
                
             {markers}
-
+            
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
+
             
         </div>
     )
